0c7b27e5f7898076b7a568d143603aa4da3c3ee3
kai zhu <kaizhu256@gmail.com>
2015-08-11 15:18:13 +0000

    ./emsdk install emscripten-tag-1.34.4-64bit

---
+++ emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/data/src/emsdk_portable/emscripten/tag-1.34.4/tools/optimizer/optimizer.cpp" "/mnt/data/src/emsdk_portable/emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/optimizer.cpp.o"
+  "/mnt/data/src/emsdk_portable/emscripten/tag-1.34.4/tools/optimizer/parser.cpp" "/mnt/data/src/emsdk_portable/emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/parser.cpp.o"
+  "/mnt/data/src/emsdk_portable/emscripten/tag-1.34.4/tools/optimizer/simple_ast.cpp" "/mnt/data/src/emsdk_portable/emscripten/tag-1.34.4_64bit_optimizer/CMakeFiles/optimizer.dir/simple_ast.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
